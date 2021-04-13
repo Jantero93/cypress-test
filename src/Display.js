3d29509c3e4085d0fbc073e5b5c25fa631e56d8f
Joonas Pennanen <joonas.pennanen@tuni.fi>
2021-04-13 10:15:25 +0300

    form

---
+++ src/Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Display(props) {
+  console.log('props', props);
+
+  return (
+    <div>
+      <h1>{props.firstName}</h1>
+      <h1>{props.lastName}</h1>
+      <h1>{props.userName}</h1>
+    </div>
+  );
+}
---
+++ src/Form.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import Display from './Display';
+
+export default class Form extends React.Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    userName: '',
+  };
+
+  changeName = (e) => {
+    console.log('name', e);
+    this.setState({
+      firstName: e.target.value,
+    });
+  };
+
+  changeLast = (e) => {
+    this.setState({
+      lastName: e.target.value,
+    });
+  };
+
+  changeUser = (e) => {
+    this.setState({
+      userName: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
+    console.log('e', e);
+    console.log('name', this.state.firstName);
+    console.log('lastname', this.state.lastName);
+    console.log('user', this.state.userName);
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <form>
+            <input
+              name="firstName"
+              placeholder="First name"
+              value={this.state.firstName}
+              onChange={(e) => this.changeName(e)}
+            />
+            <br />
+            <input
+              name="lastName"
+              placeholder="Last name"
+              value={this.state.lastName}
+              onChange={(e) => this.changeLast(e)}
+            />
+            <br />
+            <input
+              name="username"
+              placeholder="Username"
+              value={this.state.userName}
+              onChange={(e) => this.changeUser(e)}
+            />
+            <br />
+            <button onClick={(e) => this.onSubmit(e)}>Submit</button>
+          </form>
+        </div>
+        <Display
+          firstName={this.props.firstName}
+          userName={this.props.userName}
+          lastName={this.props.lastName}
+        />
+      </div>
+    );
+  }
+}
